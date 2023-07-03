612c3fc1aede31ed1a23b17eb78c930dc7251104
TomasCosta1 <totocosta3@gmail.com>
2023-07-03 12:06:50 -0300

    Mejora footer y fuentes oscuras

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,8 @@ export default function Footer() {
       <p>
         <span className='titulo'>Maestro Pokemon: </span><a href="https://tomascosta.tech" target='_blank'>Tomas Costa</a>
       </p>
+      <p className="frontText">Frontend III</p>
+      <img className="logoDigital" src="src/assets/logoDigitalHouse.png" alt="Logo digital house" />
     </footer>
   )
 }
